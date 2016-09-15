73822ae36534dca493f81cffdef4a8971e4f62e5
Christoph Anderson <cwolfanderson@gmail.com>
2016-09-14 22:55:29 -0700

    Added Magnet Poetry

---
+++ pages/react_js/js/hello_world.js
@@ -0,0 +1,14 @@
+// alert('hi');
+// 
+// $(function() {
+//   
+//   alert('ho');
+//   
+//   ReactDOM.render(
+//     <h1>Hello, world!</h1>,
+//     document.getElementById('example')
+//   );
+//   
+// });
+
+alert('hello');
